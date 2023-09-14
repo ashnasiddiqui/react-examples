bdfb3307cbc32babab33971fb3f74ea76e2559bf
Ashna Siddiqui <ashna@huraininfotech.com>
2023-09-14 14:31:58 +0530

    Updated header.js

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Container, Nav } from "react-bootstrap";
 import { Navbar } from "react-bootstrap";
-import './Header.css'
+import "./Header.css";
 function Header() {
   return (
     <Navbar expand="lg" className="navbar-custom">
       <Nav>
-          <Navbar.Brand className="nav"><img src="logo.jpg" width="30" height="30"/>Hurain Infotech</Navbar.Brand>
-          <Nav.Link href="#" className="nav">Getting Started</Nav.Link>
-          <Nav.Link href="#" className="nav">Components</Nav.Link>
-        </Nav>
+        <Container>
+          <Navbar.Toggle className="toggle"/>
+          <Navbar.Brand className="nav brand">
+            <img src="logoimage.png" width="80" height="30" alt="logo" />
+          </Navbar.Brand>
+        </Container>
+        <Navbar.Collapse>
+          <Nav.Link href="#" className="nav">
+            Dashboard
+          </Nav.Link>
+          <Nav.Link href="#" className="nav">
+            About
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Nav>
     </Navbar>
   );
 }
